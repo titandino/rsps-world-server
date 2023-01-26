e04da13db081c82552e44ceca6456c2372e51d2b
Trenton Kress <trenton.kress@gmail.com>
2023-01-26 10:39:12 -0800

    Update plugins to new system

--- src/main/java/com/rs/rsps/Home.java
+++ src/main/java/com/rs/rsps/Home.java
@@ -14,7 +14,6 @@ import com.rs.lib.game.WorldObject;
 import com.rs.lib.game.WorldTile;
 import com.rs.plugin.annotations.PluginEventHandler;
 import com.rs.plugin.annotations.ServerStartupEvent;
-import com.rs.plugin.events.ObjectClickEvent;
 import com.rs.plugin.handlers.ObjectClickHandler;
 import com.rs.rsps.teleports.BossTeleport;
 import com.rs.rsps.teleports.SlayerTeleport;
@@ -50,29 +49,26 @@ public class Home {
 		spawnNPC(13727, WorldTile.of(3091, 3505, 0), Direction.SOUTH, false);
 	}
 
-	public static ObjectClickHandler handleShops = new ObjectClickHandler(new Object[] { 18789 }, WorldTile.of(3095, 3499, 0)) {
-		@Override
-		public void handle(ObjectClickEvent e) {
-			e.getPlayer().startConversation(new Dialogue().addOptions("Which shops would you like to see?", new Options() {
-				@Override
-				public void create() {
-					option("Melee shops", new Dialogue().addOptions("Which shop would you like to see?", ops -> {
-						ops.add("Melee weapons", () -> ShopsHandler.openShop(e.getPlayer(), "hs_melee_weapons"));
-						ops.add("Melee armor", () -> ShopsHandler.openShop(e.getPlayer(), "hs_melee_armor"));
-					}));
-					option("Ranged shops", new Dialogue().addOptions("Which shop would you like to see?", ops -> {
-						ops.add("Ranged weapons", () -> ShopsHandler.openShop(e.getPlayer(), "hs_range_weapons"));
-						ops.add("Ranged armor", () -> ShopsHandler.openShop(e.getPlayer(), "hs_range_armor"));
-					}));
-					option("Magic shops", new Dialogue().addOptions("Which shop would you like to see?", ops -> {
-						ops.add("Magic weapons", () -> ShopsHandler.openShop(e.getPlayer(), "hs_mage_weapons"));
-						ops.add("Magic armor", () -> ShopsHandler.openShop(e.getPlayer(), "hs_mage_armor"));
-					}));
-					option("Supply shop", () -> ShopsHandler.openShop(e.getPlayer(), "hs_supplies"));
-				}
-			}));
-		}
-	};
+	public static ObjectClickHandler handleShops = new ObjectClickHandler(new Object[] { 18789 }, WorldTile.of(3095, 3499, 0), e -> {
+		e.getPlayer().startConversation(new Dialogue().addOptions("Which shops would you like to see?", new Options() {
+			@Override
+			public void create() {
+				option("Melee shops", new Dialogue().addOptions("Which shop would you like to see?", ops -> {
+					ops.add("Melee weapons", () -> ShopsHandler.openShop(e.getPlayer(), "hs_melee_weapons"));
+					ops.add("Melee armor", () -> ShopsHandler.openShop(e.getPlayer(), "hs_melee_armor"));
+				}));
+				option("Ranged shops", new Dialogue().addOptions("Which shop would you like to see?", ops -> {
+					ops.add("Ranged weapons", () -> ShopsHandler.openShop(e.getPlayer(), "hs_range_weapons"));
+					ops.add("Ranged armor", () -> ShopsHandler.openShop(e.getPlayer(), "hs_range_armor"));
+				}));
+				option("Magic shops", new Dialogue().addOptions("Which shop would you like to see?", ops -> {
+					ops.add("Magic weapons", () -> ShopsHandler.openShop(e.getPlayer(), "hs_mage_weapons"));
+					ops.add("Magic armor", () -> ShopsHandler.openShop(e.getPlayer(), "hs_mage_armor"));
+				}));
+				option("Supply shop", () -> ShopsHandler.openShop(e.getPlayer(), "hs_supplies"));
+			}
+		}));
+	});
 
 	private static Teleport[] PVP_TELEPORTS = {
 			new Teleport("Mage bank (SAFE)", WorldTile.of(2539, 4715, 0)),
@@ -92,92 +88,80 @@ public class Home {
 			null
 	};
 	
-	public static ObjectClickHandler healingWell = new ObjectClickHandler(new Object[] { 28715 }, WorldTile.of(3080, 3487, 0)) {
-		@Override
-		public void handle(ObjectClickEvent e) {
-			e.getPlayer().sendMessage("You feel refreshed.");
-			e.getPlayer().getPrayer().restorePrayer(e.getPlayer().getSkills().getLevelForXp(Constants.PRAYER) * 10);
-			e.getPlayer().getPoison().reset();
-			e.getPlayer().setHitpoints(e.getPlayer().getMaxHitpoints());
-			e.getPlayer().refreshHitPoints();
-			e.getPlayer().getSkills().set(Constants.SUMMONING, e.getPlayer().getSkills().getLevelForXp(Constants.SUMMONING));
-			e.getPlayer().sendOptionDialogue("Would you like to switch up your prayers or magic?", o1 -> {
-				o1.add("Select new magic book", new Dialogue().addOptions(o2 -> {
-					o2.add("Modern", () -> e.getPlayer().getCombatDefinitions().setSpellbook(Spellbook.MODERN));
-					o2.add("Ancient", () -> e.getPlayer().getCombatDefinitions().setSpellbook(Spellbook.ANCIENT));
-					o2.add("Lunar", () -> e.getPlayer().getCombatDefinitions().setSpellbook(Spellbook.LUNAR));
-				}));
-				o1.add("Select new prayer book", new Dialogue().addOptions(o2 -> {
-					o2.add("Modern", () -> e.getPlayer().getPrayer().setPrayerBook(false));
-					o2.add("Ancient curses", () -> e.getPlayer().getPrayer().setPrayerBook(true));
-				}));
-			});
-		}
-	};
-
-	public static ObjectClickHandler pvpPortal = new ObjectClickHandler(new Object[] { 46935 }, WorldTile.of(3093, 3506, 0)) {
-		@Override
-		public void handle(ObjectClickEvent e) {
-			e.getPlayer().startConversation(new Dialogue()
-					.addSimple("<col=FF0000><shad=000000>THIS PORTAL LEADS TO WILDERNESS LOCATIONS. USE AT YOUR OWN RISK. THE LEVEL OF WILDERNESS YOU WILL BE TELEPORTED TO ARE DISPLAYED ON EACH TELEPORT.")
-					.addOptions("Where would you like to go?", new Options() {
-						@Override
-						public void create() {
-							for (Teleport t : PVP_TELEPORTS)
-								if (t != null)
-									option(t.getName(), new Dialogue().addNext(() -> t.teleport(e.getPlayer())));
-								else
-									option("Nowhere.", () -> {});
-						}
-					}));
-		}
-	};
-
-	public static ObjectClickHandler bossPortal = new ObjectClickHandler(new Object[] { 46933 }, WorldTile.of(3083, 3492, 0)) {
-		@Override
-		public void handle(ObjectClickEvent e) {
-			if (e.getPlayer().getBossTask() == null || e.getPlayer().getBossTask().getTask() == null) {
-				e.getPlayer().sendMessage("You don't have a boss task to teleport to currently.");
-				return;
-			}
-			Teleport[] teleports = BossTeleport.forBoss(e.getPlayer().getBossTask().getTask()).getTeleports();
-			if (teleports == null || teleports.length <= 0) {
-				e.getPlayer().sendMessage("Your task doesn't have any teleports configured for it. Suggest it in discord.");
-				return;
-			}
-			e.getPlayer().startConversation(new Dialogue().addOptions("Where would you like to go?", new Options() {
-				@Override
-				public void create() {
-					for (Teleport t : teleports)
-						option(t.getName(), new Dialogue().addNext(() -> t.teleport(e.getPlayer())));
-					option("Nowhere.");
-				}
+	public static ObjectClickHandler healingWell = new ObjectClickHandler(new Object[] { 28715 }, WorldTile.of(3080, 3487, 0), e -> {
+		e.getPlayer().sendMessage("You feel refreshed.");
+		e.getPlayer().getPrayer().restorePrayer(e.getPlayer().getSkills().getLevelForXp(Constants.PRAYER) * 10);
+		e.getPlayer().getPoison().reset();
+		e.getPlayer().setHitpoints(e.getPlayer().getMaxHitpoints());
+		e.getPlayer().refreshHitPoints();
+		e.getPlayer().getSkills().set(Constants.SUMMONING, e.getPlayer().getSkills().getLevelForXp(Constants.SUMMONING));
+		e.getPlayer().sendOptionDialogue("Would you like to switch up your prayers or magic?", o1 -> {
+			o1.add("Select new magic book", new Dialogue().addOptions(o2 -> {
+				o2.add("Modern", () -> e.getPlayer().getCombatDefinitions().setSpellbook(Spellbook.MODERN));
+				o2.add("Ancient", () -> e.getPlayer().getCombatDefinitions().setSpellbook(Spellbook.ANCIENT));
+				o2.add("Lunar", () -> e.getPlayer().getCombatDefinitions().setSpellbook(Spellbook.LUNAR));
 			}));
-		}
-	};
-
-	public static ObjectClickHandler slayerPortal = new ObjectClickHandler(new Object[] { 46934 }, WorldTile.of(3095, 3483, 0)) {
-		@Override
-		public void handle(ObjectClickEvent e) {
-			if (e.getPlayer().getSlayer().getTask() == null) {
-				e.getPlayer().sendMessage("You don't have a slayer task to teleport to currently.");
-				return;
-			}
-			Teleport[] teleports = SlayerTeleport.forMonster(e.getPlayer().getSlayer().getTask().getMonster()).getTeleports();
-			if (teleports == null || teleports.length <= 0) {
-				e.getPlayer().sendMessage("Your task doesn't have any teleports configured for it. Suggest it in discord.");
-				return;
-			}
-			e.getPlayer().startConversation(new Dialogue().addOptions("Where would you like to go?", new Options() {
-				@Override
-				public void create() {
-					for (Teleport t : teleports)
-						option(t.getName(), new Dialogue().addNext(() -> t.teleport(e.getPlayer())));
-					option("Nowhere.");
-				}
+			o1.add("Select new prayer book", new Dialogue().addOptions(o2 -> {
+				o2.add("Modern", () -> e.getPlayer().getPrayer().setPrayerBook(false));
+				o2.add("Ancient curses", () -> e.getPlayer().getPrayer().setPrayerBook(true));
 			}));
+		});
+	});
+
+	public static ObjectClickHandler pvpPortal = new ObjectClickHandler(new Object[] { 46935 }, WorldTile.of(3093, 3506, 0), e -> {
+		e.getPlayer().startConversation(new Dialogue()
+				.addSimple("<col=FF0000><shad=000000>THIS PORTAL LEADS TO WILDERNESS LOCATIONS. USE AT YOUR OWN RISK. THE LEVEL OF WILDERNESS YOU WILL BE TELEPORTED TO ARE DISPLAYED ON EACH TELEPORT.")
+				.addOptions("Where would you like to go?", new Options() {
+					@Override
+					public void create() {
+						for (Teleport t : PVP_TELEPORTS)
+							if (t != null)
+								option(t.getName(), new Dialogue().addNext(() -> t.teleport(e.getPlayer())));
+							else
+								option("Nowhere.", () -> {});
+					}
+				}));
+	});
+
+	public static ObjectClickHandler bossPortal = new ObjectClickHandler(new Object[] { 46933 }, WorldTile.of(3083, 3492, 0), e -> {
+		if (e.getPlayer().getBossTask() == null || e.getPlayer().getBossTask().getTask() == null) {
+			e.getPlayer().sendMessage("You don't have a boss task to teleport to currently.");
+			return;
 		}
-	};
+		Teleport[] teleports = BossTeleport.forBoss(e.getPlayer().getBossTask().getTask()).getTeleports();
+		if (teleports == null || teleports.length <= 0) {
+			e.getPlayer().sendMessage("Your task doesn't have any teleports configured for it. Suggest it in discord.");
+			return;
+		}
+		e.getPlayer().startConversation(new Dialogue().addOptions("Where would you like to go?", new Options() {
+			@Override
+			public void create() {
+				for (Teleport t : teleports)
+					option(t.getName(), new Dialogue().addNext(() -> t.teleport(e.getPlayer())));
+				option("Nowhere.");
+			}
+		}));
+	});
+
+	public static ObjectClickHandler slayerPortal = new ObjectClickHandler(new Object[] { 46934 }, WorldTile.of(3095, 3483, 0), e -> {
+		if (e.getPlayer().getSlayer().getTask() == null) {
+			e.getPlayer().sendMessage("You don't have a slayer task to teleport to currently.");
+			return;
+		}
+		Teleport[] teleports = SlayerTeleport.forMonster(e.getPlayer().getSlayer().getTask().getMonster()).getTeleports();
+		if (teleports == null || teleports.length <= 0) {
+			e.getPlayer().sendMessage("Your task doesn't have any teleports configured for it. Suggest it in discord.");
+			return;
+		}
+		e.getPlayer().startConversation(new Dialogue().addOptions("Where would you like to go?", new Options() {
+			@Override
+			public void create() {
+				for (Teleport t : teleports)
+					option(t.getName(), new Dialogue().addNext(() -> t.teleport(e.getPlayer())));
+				option("Nowhere.");
+			}
+		}));
+	});
 
 	public static NPC spawnNPC(int id, WorldTile tile, String name, Direction direction, boolean randomWalk) {
 		NPC npc = new NPC(id, tile);
